146452d2f6b19e9a2c885edd70e2a8fffe321da2
AlexLoitzl <alexander.loitzl@gmail.com>
2024-09-05 15:20:04 +0200

    Add interesting default script as comment

--- test1.sh
+++ test1.sh
@@ -6,4 +6,22 @@ INPUTFILE=$1
 
 # ...add your tests here, exit with zero/non-zero for success/failure...
 
+# E.g. Simply output the files
+# Use a temporary file as a counter
+# COUNT_FILE="count"
+# TEST_DIR="tests"
+# mkdir -p tests
+
+# if [ ! -f ${COUNT_FILE} ]; then
+#   echo "0" > $COUNT_FILE
+# fi
+# # Increment counter and echo to file
+# exec {counterfd}<$COUNT_FILE
+# flock -w 5 "$counterfd"
+# COUNTER=$(cat $COUNT_FILE)
+# cat $INPUTFILE > $TEST_DIR/test$COUNTER
+# COUNTER=$(( COUNTER +1 ))
+# echo "$COUNTER" >$COUNT_FILE
+# exec {counterfd}<&-
+
 exit 0
